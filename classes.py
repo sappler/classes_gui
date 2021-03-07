80f61bc009ad5f2c7a5a119f8343278c9205d39b
Steven Appler <44624294+sappler@users.noreply.github.com>
2021-03-07 13:01:02 -0800

    added descriptors for input

--- classes.py
+++ classes.py
@@ -20,7 +20,7 @@ def click():
         #row=file[file['subject']==entered_sub,file['number']==entered_num]
         qstring=('subject=="{}" and number=="{}"')
         row=file.query(qstring.format(entered_sub,entered_num))
-        print(row['description'])
+        #print(row['description'])
         name.configure(text=row['title'].values[0])
         desc.configure(text=row['description'].values[0])
     except:
@@ -35,7 +35,7 @@ Button(window,text="EXIT",width=4,command=close_window).pack(side=TOP,anchor=NE)
 #creates text object
 MyTitle = Label(window,text="My first app",font="Helvetica 16 bold")
 #puts text obj in the window
-MyTitle.pack()
+
 #creates button that calls the earlier function
 #create empty text obj. our random function later makes this text non-null and makes it display
 #picture
@@ -45,28 +45,35 @@ MyTitle.pack()
 #puts image in frame
 #p=Label(window,image=pic,bg="black").grid(row=1,column=0,sticky=W)
 
-Prompt = Label(window,text="Enter class subject",font="none 12 bold")
-Prompt.pack()
-Prompt = Label(window,text="Enter class #",font="none 12 bold")
-Prompt.pack()
+Promptsub = Label(window,text="Enter class subject",font="none 12 bold")
+
+Promptnum = Label(window,text="Enter class #",font="none 12 bold")
+
 
 #input 
 textentry = Entry(window,width=20,bg="white")
-textentry.pack()
+
 textentrynum = Entry(window,width=20,bg="white")
-textentrynum.pack()
+
 
 #output=Text(window,width=30,height=20,wrap=WORD,background="white")
 #output.pack()
 name = Label(window, font="Helvetica 12 bold",wraplength=300, justify="center")
-name.pack()
+
 desc = Label(window, font="Helvetica 12 bold",wraplength=300, justify="center")
-desc.pack()
-
-Button(window,text="SUBMIT",width=6,command=click).pack()
 
 
+submit =  Button(window,text="SUBMIT",width=6,command=click)
 
 
 
+MyTitle.pack(fill=X)
+Promptsub.pack(fill=X)
+textentry.pack(fill=X)
+Promptnum.pack(fill=X)
+
+textentrynum.pack(fill=X)
+name.pack(fill=X)
+desc.pack(fill=X)
+submit.pack()
 window.mainloop()
