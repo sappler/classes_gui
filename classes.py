9a13aee8a23663370fa5f285e627d467d9e7a027
Steven Appler <44624294+sappler@users.noreply.github.com>
2021-03-07 16:53:12 -0800

    major upgrades, classification, style
    -put all objects into a main class for easier creation/deletion
    -added picture
    -added colored buttons
    -added "go to webpage" option

--- classes.py
+++ classes.py
@@ -1,79 +1,133 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import pandas as pd
-import fileinput
-pd.set_option("display.max_rows", None, "display.max_columns", None)
-pd.set_option('display.max_colwidth', -1)
-#shortcut to use a window
-window=Tk()
-window.title("example app")
-#window.configure(background="black")
-file=pd.read_csv('db.csv',sep='#',engine='python',quoting=3)
-#print(file[file['subject']=='CSE',file['number']=='10'])
-print(file.query('subject=="CSE" and number=="3"'))
-#print(row['description'])
+import webbrowser
 
-def click():
-    entered_sub=textentry.get().upper()
-    entered_num=textentrynum.get().upper()
-    try:
-        #row=file[file['subject']==entered_sub,file['number']==entered_num]
-        qstring=('subject=="{}" and number=="{}"')
-        row=file.query(qstring.format(entered_sub,entered_num))
-        #print(row['description'])
-        name.configure(text=row['title'].values[0])
-        desc.configure(text=row['description'].values[0])
-    except:
-        name.configure("try to spellcheck")
+
+window=None
+file=None
+
 
 def close_window():
     window.destroy()
-    exit()
-   
-#@leave window
-Button(window,text="EXIT",width=4,command=close_window).pack(side=TOP,anchor=NE)
-#creates text object
-MyTitle = Label(window,text="My first app",font="Helvetica 16 bold")
-#puts text obj in the window
+    exit()    
 
-#creates button that calls the earlier function
-#create empty text obj. our random function later makes this text non-null and makes it display
-#picture
-#PhotoImage object created. Uses PIL image opener inside.
-#pic=ImageTk.PhotoImage(Image.open("me.gif").resize(400,400))
-#grid: sticky = N,W,E,(S)outh
-#puts image in frame
-#p=Label(window,image=pic,bg="black").grid(row=1,column=0,sticky=W)
+class Elements:
+    def goto(self):
+        webbrowser.open(self.link,new=1)
+    
+    def click(self,event=None):
+            text=self.textentry.get().split(' ')
+            try:
+                entered_sub=text[0].upper()
+                entered_num=text[1].upper()
+                qstring=('subject=="{}" and number=="{}"')
+                row=file.query(qstring.format(entered_sub,entered_num))
+                self.name.configure(text=row['title'].values[0])
+                self.desc.configure(text=row['description'].values[0])
+                self.link='https://courses.soe.ucsc.edu/courses/' + entered_sub + entered_num
+                self.urlbutton.pack()
+            except:
+                self.name.configure(text="That is not a class at UCSC.")
+                self.clear()
+                return
+            try:
+                self.quarters.configure(text=row['quarters'].values[0])
+                self.quarters_label.configure(text='Quarters:')
+            except:
+                self.quarters.configure(text='')
+                self.quarters_label.configure(text='')
+            try: 
+                self.instructors.configure(text=row['instructors'].values[0])
+                self.instructors_label.configure(text='Instructors:')
+            except:
+                self.instructors.configure(text='')
+                self.instructors_label.configure(text='')
+    
+    #packs all objects
+    def showall(self):
+        self.exit.pack(side=TOP,anchor=NE)
+        self.p.pack()
+        self.Title.pack(fill=X)
+        self.Prompt.pack(fill=X)
+        self.textentry.pack()
+        # sets cursor inside first text box
+        self.textentry.focus()
+        self.submit.pack()
+        self.name.pack()
+        self.desc.pack()
+        self.instructors_label.pack()
+        self.instructors.pack()
+        self.quarters_label.pack()
+        self.quarters.pack()
+    
+    #destroys all objects except exit button
+    def killall(self):
+        self.p.pack_forget()
+        self.Title.pack_forget()
+        self.Prompt.pack_forget()
+        self.textentry.pack_forget()
+        self.submit.pack_forget()
+        self.name.pack_forget()
+        self.desc.pack_forget()
+        self.instructors_label.pack_forget()
+        self.instructors.pack_forget()
+        self.quarters_label.pack_forget()
+        self.quarters.pack_forget()
+    
+    #clears the output textboxes
+    def clear(self):
+        self.urlbutton.pack_forget()
+        self.desc.configure(text='')
+        self.quarters.configure(text='')
+        self.quarters_label.configure(text='')
+        self.instructors.configure(text='')
+        self.instructors_label.configure(text='')
+    
+    #sets up all objects
+    def __init__(self):
+        #makes enter button 'press' the submit button
+        window.bind('<Return>',self.click)
+        self.link=''
+        
+        
+        #opens image as pillow object
+        pic_temp=Image.open("smal.png")
+        pic_temp=pic_temp.resize((175,175), Image.ANTIALIAS)
+        self.pic=ImageTk.PhotoImage(pic_temp)
+        #puts image in 'frame' (has to be done to display PIL image
+        self.p =Label(window,image=self.pic)
+        
+        
+        #creates text object MyTitle. see options
+        self.Title = Label(window,text="UCSC offline class lookup",font="Helvetica 16 bold")
+        self.Prompt = Label(window,text="Enter class subject, then a space, followed by class number",font="none 12 bold")
+        
+        #input box
+        self.textentry = Entry(window,width=20)
 
-Promptsub = Label(window,text="Enter class subject",font="none 12 bold")
-
-Promptnum = Label(window,text="Enter class #",font="none 12 bold")
-
-
-#input 
-textentry = Entry(window,width=20,bg="white")
-
-textentrynum = Entry(window,width=20,bg="white")
-
-
-#output=Text(window,width=30,height=20,wrap=WORD,background="white")
-#output.pack()
-name = Label(window, font="Helvetica 12 bold",wraplength=300, justify="center")
-
-desc = Label(window, font="Helvetica 12 bold",wraplength=300, justify="center")
-
-
-submit =  Button(window,text="SUBMIT",width=6,command=click)
-
-
-
-MyTitle.pack(fill=X)
-Promptsub.pack(fill=X)
-textentry.pack(fill=X)
-Promptnum.pack(fill=X)
-
-textentrynum.pack(fill=X)
-name.pack(fill=X)
-desc.pack(fill=X)
-submit.pack()
-window.mainloop()
+        #all output text boxes
+        self.name = Label(window, font="Helvetica 12 bold",justify='left')
+        self.desc = Label(window, font="Helvetica 12",wraplength=600,justify='left')
+        self.quarters = Label(window, font="Helvetica 12", anchor='w')
+        self.instructors = Label(window, font="Helvetica 12",wraplength=600)
+        self.quarters_label = Label(window, font="Helvetica 12 bold")
+        self.instructors_label = Label(window,font="Helvetica 12 bold")
+        
+        #buttons: submit, page, and exit buttons
+        self.exit = Button(window,text="EXIT",font="bold",width=4,command=close_window,background="red",foreground="white")
+        self.submit =  Button(window,text="SUBMIT",width=6,command=self.click)
+        self.urlbutton= Button(window,text="Go to Class page",command=self.goto,background="blue",foreground="white")
+    
+    
+if __name__=="__main__":
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    pd.set_option('display.max_colwidth', None)
+    file=pd.read_csv('db.csv',sep='#',engine='python',quoting=3)
+    window=Tk()
+    window.title("UCSC Class Lookup")
+    #width/height
+    window.geometry("1000x750")
+    myE=Elements()
+    myE.showall()
+    window.mainloop()
